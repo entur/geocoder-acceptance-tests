1fa461aa574cae24a16db52cc58065f035aad96f
Tom Erik Støwer <testower@gmail.com>
2022-12-13 12:22:13 +0100

    Wait for elasticsearch in test container instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -e
+
+: ${LOCAL=""}
+
 log_url="https://console.cloud.google.com/logs/query;query=resource.type%3D%22k8s_container%22%0Aresource.labels.project_id%3D%22$GOOGLE_PROJECT_ID%22%0Aresource.labels.pod_name%3D~%22geocoder-acceptance-tests-.*%22;timeRange=PT1H?project=$GOOGLE_PROJECT_ID"
 
 function send_test_failure_notification {
@@ -12,6 +16,13 @@ function send_test_failure_notification {
             $SLACK_URL
     fi
 }
+
+if [[ ! -z ${LOCAL} ]]; then
+  until $(curl --output /dev/null --silent --head --fail http://localhost:9200/_cluster/health?wait_for_status=yellow&timeout=120s); do
+    printf '.'
+    sleep 5
+  done
+fi
 
 yarn run $1
 
