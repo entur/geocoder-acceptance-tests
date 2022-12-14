6a049719ab339bc14292f141f5c80a1e2f1b8697
Tom Erik Støwer <testower@gmail.com>
2022-12-14 08:54:12 +0100

    Send test result message to pubsub

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 : ${LOCAL=""}
+: ${PUBSUB_TOPIC=""}
 
 log_url="https://console.cloud.google.com/logs/query;query=resource.type%3D%22k8s_container%22%0Aresource.labels.project_id%3D%22$GOOGLE_PROJECT_ID%22%0Aresource.labels.pod_name%3D~%22geocoder-acceptance-tests-.*%22;timeRange=PT1H?project=$GOOGLE_PROJECT_ID"
 
@@ -14,6 +15,12 @@ function send_test_failure_notification {
         curl -X POST -H 'Content-type: application/json' \
             --data '{"text":":warning: Geocoder smoke tests had failures. See logs for details: '$log_url'"}' \
             $SLACK_URL
+    fi
+}
+
+function send_test_result_message(success) {
+    if [[ ! -z ${PUBSUB_TOPIC} ]]; then
+      gcloud pubsub topics publish $PUBSUB_TOPIC --message "{\"success\": $success}"
     fi
 }
 
@@ -29,14 +36,12 @@ fi
 
 yarn run $1
 
-#if [[ ! -z ${LOCAL} ]]; then
-  # send pubsub message to kakka with test results
-#fi
-
 if [ $? -eq 0 ]
 then
   echo "Successfully test run"
+  send_test_result_message true
 else
   echo "Some tests failed" >&2
   send_test_failure_notification
+  send_test_result_message false
 fi
